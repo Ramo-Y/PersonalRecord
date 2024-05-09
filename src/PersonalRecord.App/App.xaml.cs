17d82474bd37721ba857ec2081d61d18a29f172a
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-05-09 16:17:14 +0200

    Add empty MAUI app

---
+++ src/PersonalRecord.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalRecord.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/PersonalRecord.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonalRecord.App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
