77d16bf37eccb9fb47183c77bec7fc966efa7c29
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-05-09 17:03:52 +0200

    Add domain class library

---
+++ src/PersonalRecord.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace PersonalRecord.Domain
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
---
+++ src/PersonalRecord.Domain/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace PersonalRecord.Domain
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
