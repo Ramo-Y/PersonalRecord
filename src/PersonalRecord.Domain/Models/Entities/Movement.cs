7a4840885336c8faa22b7402ce76e710f99d7a6a
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-05-09 18:21:01 +0200

    Add entitie classes

---
+++ src/PersonalRecord.Domain/Models/Entities/Movement.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonalRecord.Domain.Models.Entities
+{
+    public class Movement
+    {
+        public Guid MovementID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ src/PersonalRecord.Domain/Models/Entities/PersonalRecord.cs
@@ -0,0 +1,13 @@
+﻿namespace PersonalRecord.Domain.Models.Entities
+{
+    public class PersonalRecord
+    {
+        public Guid PersonalRecordID { get; set; }
+
+        public float Value { get; set; }
+
+        public Movement Movement { get; set; }
+
+        public Guid MovementID_FK { get; set; }
+    }
+}
