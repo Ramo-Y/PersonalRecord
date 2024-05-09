b5227f4b65046e53f7f1a7f4ae9fb31b1951e841
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-05-09 18:21:31 +0200

    Add database project

---
+++ src/PersonalRecord.Database/Models/PersonalRecordContext.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalRecord.Database.Models
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class PersonalRecordContext : DbContext
+    {
+        public PersonalRecordContext(DbContextOptions<PersonalRecordContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ src/PersonalRecord.Database/Platforms/Tizen/PlatformClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PersonalRecord.Database
+{
+    // All the code in this file is only included on Tizen.
+    public class PlatformClass1
+    {
+    }
+}
