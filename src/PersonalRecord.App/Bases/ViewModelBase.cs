d33cfdf80ee5bf62888d323f86b3d4d5f45ecb3a
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-05-10 17:33:59 +0200

    Cleanup code

--- src/PersonalRecord.App/Bases/ViewModelBase.cs
+++ src/PersonalRecord.App/Bases/ViewModelBase.cs
@@ -5,6 +5,7 @@
     public abstract class ViewModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
         protected virtual void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
