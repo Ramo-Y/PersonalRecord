7dd0665ea9d19278793e4eeddc12c95c2c1b7648
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-05-11 12:57:36 +0200

    Add navigationservice

---
+++ src/PersonalRecord.App/Interfaces/INavigationService.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonalRecord.App.Interfaces
+{
+    public interface INavigationService
+    {
+        Task GoToAsync(ShellNavigationState state);
+
+        Task GoBackAsync();
+    }
+}
---
+++ src/PersonalRecord.App/Services/NavigationService.cs
@@ -0,0 +1,13 @@
+﻿namespace PersonalRecord.App.Services
+{
+    using Microsoft.Maui.Controls;
+    using PersonalRecord.App.Interfaces;
+    using System.Threading.Tasks;
+
+    public class NavigationService : INavigationService
+    {
+        public Task GoBackAsync() => Shell.Current.GoToAsync("..");
+
+        public Task GoToAsync(ShellNavigationState state) => Shell.Current.GoToAsync(state);
+    }
+}
