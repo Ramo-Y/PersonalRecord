6a63da6b5ca18f4d812f518636499e109a3a43c0
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-06-08 19:09:23 +0200

    Implement MainView and MovementRecordsView

--- src/PersonalRecord.App/ViewModels/MainViewModel.cs
+++ src/PersonalRecord.App/ViewModels/MainViewModel.cs
@@ -3,55 +3,14 @@
     using CommunityToolkit.Mvvm.ComponentModel;
     using CommunityToolkit.Mvvm.Input;
     using PersonalRecord.App.Interfaces;
-    using PersonalRecord.Domain.Models.Entities;
-    using System.Collections.ObjectModel;
 
     public partial class MainViewModel : ObservableObject
     {
         private readonly INavigationService _navigationService;
 
-        private ObservableCollection<MovementRecord> _movementRecords;
-
-        private ObservableCollection<Movement> _movements;
-
         public MainViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
-
-            // TODO: Load from Firebase
-            Movements =
-            [
-                new()
-                {
-                    Name = "Deadlift",
-                },
-                new()
-                {
-                    Name = "Backsquat",
-                }
-            ];
-
-            MovementRecords =
-            [
-                new()
-                {
-                    Date = DateTime.Now,
-                    Reps = 1,
-                    Weight = 100,
-                    Movement = Movements?.FirstOrDefault()
-                }
-            ];
-        }
-        public ObservableCollection<MovementRecord> MovementRecords
-        {
-            get => _movementRecords;
-            set => SetProperty(ref _movementRecords, value);
-        }
-
-        public ObservableCollection<Movement> Movements
-        {
-            get => _movements;
-            set => SetProperty(ref _movements, value);
         }
 
         [RelayCommand]
@@ -61,9 +20,9 @@
         }
 
         [RelayCommand]
-        public async Task GoToMovementRecordDetailsViewAsync()
+        public async Task GoToMovementRecordsViewAsync()
         {
-            await _navigationService.GoToAsync(Routes.MovementRecordDetailView);
+            await _navigationService.GoToAsync(Routes.MovementRecordsView);
         }
     }
 }
--- src/PersonalRecord.App/ViewModels/MovementRecordsViewModel.cs
+++ src/PersonalRecord.App/ViewModels/MovementRecordsViewModel.cs
@@ -1,23 +1,64 @@
 ﻿namespace PersonalRecord.App.ViewModels
 {
     using CommunityToolkit.Mvvm.ComponentModel;
+    using CommunityToolkit.Mvvm.Input;
     using PersonalRecord.App.Interfaces;
     using PersonalRecord.Domain.Models.Entities;
     using System.Collections.ObjectModel;
 
     public partial class MovementRecordsViewModel : ObservableObject
     {
+        private readonly INavigationService _navigationService;
+
         private ObservableCollection<MovementRecord> _movementRecords;
+
+        private ObservableCollection<Movement> _movements;
 
         public MovementRecordsViewModel(INavigationService navigationService)
         {
+            _navigationService = navigationService;
 
+            // TODO: Load from Firebase
+            Movements =
+            [
+                new()
+                {
+                    Name = "Deadlift",
+                },
+                new()
+                {
+                    Name = "Backsquat",
+                }
+            ];
+
+            MovementRecords =
+            [
+                new()
+                {
+                    Date = DateTime.Now,
+                    Reps = 1,
+                    Weight = 100,
+                    Movement = Movements?.FirstOrDefault()
+                }
+            ];
         }
 
         public ObservableCollection<MovementRecord> MovementRecords
         {
             get => _movementRecords;
             set => SetProperty(ref _movementRecords, value);
+        }
+
+        public ObservableCollection<Movement> Movements
+        {
+            get => _movements;
+            set => SetProperty(ref _movements, value);
+        }
+
+        [RelayCommand]
+        public async Task GoToMovementRecordDetailsViewAsync()
+        {
+            await _navigationService.GoToAsync(Routes.MovementRecordDetailView);
         }
     }
 }
