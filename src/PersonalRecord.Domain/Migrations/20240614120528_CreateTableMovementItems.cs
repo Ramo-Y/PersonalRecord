c77cdfc8c74b399393a88b876d1072718d7e4b2b
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-06-14 17:22:00 +0200

    Add migrations

---
+++ src/PersonalRecord.Domain/Migrations/20240614120528_CreateTableMovementItems.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalRecord.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableMovementItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MovementItems",
+                columns: table => new
+                {
+                    MovementID = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovementItems", x => x.MovementID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MovementItems");
+        }
+    }
+}
---
+++ src/PersonalRecord.Domain/Migrations/20240614120553_CreateTableMovementRecordItems.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalRecord.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableMovementRecordItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MovementRecordItems",
+                columns: table => new
+                {
+                    PersonalRecordID = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Weight = table.Column<float>(type: "REAL", nullable: false),
+                    Reps = table.Column<int>(type: "INTEGER", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    MovementID_FK = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovementRecordItems", x => x.PersonalRecordID);
+                    table.ForeignKey(
+                        name: "FK_MovementRecordItems_MovementItems_MovementID_FK",
+                        column: x => x.MovementID_FK,
+                        principalTable: "MovementItems",
+                        principalColumn: "MovementID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovementRecordItems_MovementID_FK",
+                table: "MovementRecordItems",
+                column: "MovementID_FK");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MovementRecordItems");
+        }
+    }
+}
