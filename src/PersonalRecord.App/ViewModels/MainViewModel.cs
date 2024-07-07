1d4c295d649f11fee69c2bcc0c21544c83a9984d
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-07-07 11:59:42 +0200

    Add open URLs for help, support and report

--- src/PersonalRecord.App/ViewModels/MainViewModel.cs
+++ src/PersonalRecord.App/ViewModels/MainViewModel.cs
@@ -2,6 +2,7 @@
 {
     using CommunityToolkit.Mvvm.ComponentModel;
     using CommunityToolkit.Mvvm.Input;
+    using PersonalRecord.Infrastructure.Constants;
     using PersonalRecord.Services.Interfaces;
 
     public partial class MainViewModel : ObservableObject
@@ -35,6 +36,27 @@
         public async Task GoToSettingsViewAsync()
         {
             await _navigationService.GoToAsync(Routes.SettingsView);
+        }
+
+        [RelayCommand]
+        public async Task OpenSupportPageAsync()
+        {
+            var uri = new Uri(EnvironmentConstants.SPONSOR_URL);
+            await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+        }
+
+        [RelayCommand]
+        public async Task OpenProjectPageAsync()
+        {
+            var uri = new Uri(EnvironmentConstants.PROJECT_URL);
+            await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+        }
+
+        [RelayCommand]
+        public async Task OpenProjectIssuesPageAsync()
+        {
+            var uri = new Uri(EnvironmentConstants.PROJECT_ISSUES_URL);
+            await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
         }
     }
 }
--- src/PersonalRecord.Infrastructure/Constants/EnvironmentConstants.cs
+++ src/PersonalRecord.Infrastructure/Constants/EnvironmentConstants.cs
@@ -2,8 +2,14 @@
 {
     public static class EnvironmentConstants
     {
-        public static string LICENSE_FILENAME = "SyncfusionLicense.txt";
+        public const string DATABASE_NAME = "PersonalRecord.db";
 
-        public static string DATABASE_NAME = "PersonalRecord.db";
+        public const string LICENSE_FILENAME = "SyncfusionLicense.txt";
+
+        public const string PROJECT_URL = "https://github.com/Ramo-Y/PersonalRecord";
+
+        public const string PROJECT_ISSUES_URL = "https://github.com/Ramo-Y/PersonalRecord/issues";
+
+        public const string SPONSOR_URL = "https://github.com/sponsors/Ramo-Y";
     }
 }
