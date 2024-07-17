2d0e6be3c3b91ad910c19a701e4bac13e05391fa
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-07-17 17:41:32 +0200

    Add custom renderer

---
+++ src/PersonalRecord.App/Renderers/CustomTextRenderer.cs
@@ -0,0 +1,16 @@
+﻿namespace PersonalRecord.App.Renderers
+{
+    using Syncfusion.Maui.DataGrid;
+
+    public class CustomTextRenderer : DataGridTextBoxCellRenderer
+    {
+        public override void OnInitializeEditView(DataColumnBase dataColumn, SfDataGridEntry view)
+        {
+            base.OnInitializeEditView(dataColumn, view);
+            if (view != null && view is Entry entry)
+            {
+                entry.Keyboard = Keyboard.Create(KeyboardFlags.CapitalizeWord);
+            }
+        }
+    }
+}
