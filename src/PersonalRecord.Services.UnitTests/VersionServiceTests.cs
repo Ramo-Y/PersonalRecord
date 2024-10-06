6c90b38c4774770439bddc989d8b17f1e6eb255d
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-10-06 11:10:22 +0200

    Add multiple tests for GetAppVersion and GetInformationalVersion

--- src/PersonalRecord.Services.UnitTests/VersionServiceTests.cs
+++ src/PersonalRecord.Services.UnitTests/VersionServiceTests.cs
@@ -6,6 +6,8 @@ namespace PersonalRecord.Services.UnitTests
     {
         private readonly IVersionService _versionService;
 
+        private const char PLUS = '+';
+
         public VersionServiceTests(IVersionService versionService)
         {
             _versionService = versionService;
@@ -22,6 +24,35 @@ namespace PersonalRecord.Services.UnitTests
 
             // assert
             Assert.True(isNotEmpty, $"The version info '{appVersion}' should not be empty");
+        }
+
+        [Fact]
+        public void GetInformationalVersion_IsNotEmpty()
+        {
+            // arrange
+            var informationalVersion = _versionService.GetInformationalVersion();
+
+            // act
+            var isNotEmpty = !string.IsNullOrEmpty(informationalVersion);
+
+            // assert
+            Assert.True(isNotEmpty, $"The informational version info '{informationalVersion}' should not be empty");
+        }
+
+        [Fact]
+        public void GetInformationalVersion_HasVersionNumberAndPlus()
+        {
+            // arrange
+            var informationalVersion = _versionService.GetInformationalVersion();
+
+            // act
+            var numberString = informationalVersion[0].ToString();
+            var isInt = int.TryParse(numberString, out var number);
+            var containsPlus = informationalVersion.Contains(PLUS);
+
+            // assert
+            Assert.True(containsPlus, $"The informational version info '{informationalVersion}' should contain a '{PLUS}'");
+            Assert.True(isInt, $"The informational version info '{informationalVersion}' should start with a number but actually starts with '{numberString}'");
         }
     }
 }
