ab91f7ca966ff15b62e9bf7c0e274bbbb171d777
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-10-25 18:47:36 +0200

    Create table ExerciseItems

---
+++ src/PersonalRecord.Domain/Migrations/20241025153401_CreateTableExerciseItems.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalRecord.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableExerciseItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExerciseItems",
+                columns: table => new
+                {
+                    ExerciseID = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ExrName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExerciseItems", x => x.ExerciseID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExerciseItems");
+        }
+    }
+}
---
+++ src/PersonalRecord.Domain/Models/Entities/Exercise.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalRecord.Domain.Models.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Exercise
+    {
+        [Key]
+        public Guid ExerciseID { get; set; }
+
+        public string ExrName { get; set; }
+    }
+}
