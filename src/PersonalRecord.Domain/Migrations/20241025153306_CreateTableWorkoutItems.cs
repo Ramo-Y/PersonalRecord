59cdab6626afd67fad3c739a05d3dd6a41e2fd9b
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-10-25 18:47:19 +0200

    Create table WorkoutItems

---
+++ src/PersonalRecord.Domain/Migrations/20241025153306_CreateTableWorkoutItems.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalRecord.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableWorkoutItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WorkoutItems",
+                columns: table => new
+                {
+                    WorkoutID = table.Column<Guid>(type: "TEXT", nullable: false),
+                    WokName = table.Column<string>(type: "TEXT", nullable: false),
+                    WokNotes = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkoutItems", x => x.WorkoutID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WorkoutItems");
+        }
+    }
+}
