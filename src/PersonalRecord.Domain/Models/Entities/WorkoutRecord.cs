2e43f32dbf65770c55600812de9a4cdffb729d2c
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-10-25 18:48:17 +0200

    Create table WorkoutRecordItems

---
+++ src/PersonalRecord.Domain/Models/Entities/WorkoutRecord.cs
@@ -0,0 +1,26 @@
+﻿namespace PersonalRecord.Domain.Models.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class WorkoutRecord
+    {
+        [Key]
+        public Guid WorkoutRecordID { get; set; }
+
+        public Guid WreWorkoutID_FK { get; set; }
+
+        [ForeignKey(nameof(WreWorkoutID_FK))]
+        public Workout Workout { get; set; }
+
+        public DateTime WreDate { get; set; }
+
+        public TimeOnly WreTime { get; set; }
+
+        public bool WreCompletedRx { get; set; }
+
+        public float WreScaledWeight { get; set; }
+
+        public string WreNotes { get; set; }
+    }
+}
