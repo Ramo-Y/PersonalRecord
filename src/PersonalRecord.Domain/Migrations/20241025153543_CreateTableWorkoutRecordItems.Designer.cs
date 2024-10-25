d596b1249e1e5c309a01ed81d2ad17a3e4af3699
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-10-25 18:48:17 +0200

    Create table WorkoutRecordItems

--- src/PersonalRecord.Domain/Migrations/20241025153543_CreateTableWorkoutRecordItems.Designer.cs
+++ src/PersonalRecord.Domain/Migrations/20241025153543_CreateTableWorkoutRecordItems.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PersonalRecord.Domain.Models.Entities;
 
@@ -10,9 +11,11 @@ using PersonalRecord.Domain.Models.Entities;
 namespace PersonalRecord.Domain.Migrations
 {
     [DbContext(typeof(PersonalRecordContext))]
-    partial class PersonalRecordContextModelSnapshot : ModelSnapshot
+    [Migration("20241025153543_CreateTableWorkoutRecordItems")]
+    partial class CreateTableWorkoutRecordItems
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
--- src/PersonalRecord.Domain/Models/Entities/PersonalRecordContext.cs
+++ src/PersonalRecord.Domain/Models/Entities/PersonalRecordContext.cs
@@ -18,5 +18,7 @@
         public DbSet<Exercise> ExerciseItems { get; set; }
 
         public DbSet<WorkoutToExercise> WorkoutToExerciseItems { get; set; }
+
+        public DbSet<WorkoutRecord> WorkoutRecordItems { get; set; }
     }
 }
