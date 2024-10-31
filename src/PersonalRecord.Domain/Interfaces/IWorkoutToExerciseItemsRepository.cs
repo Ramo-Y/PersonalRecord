158d716904b533381a2fc6b6279ee44d6b0e0e73
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-10-31 22:50:49 +0100

    Add and implement IWorkoutToExerciseItemsRepository

---
+++ src/PersonalRecord.Domain/Interfaces/IWorkoutToExerciseItemsRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonalRecord.Domain.Interfaces
+{
+    using PersonalRecord.Domain.Models.Entities;
+
+    public interface IWorkoutToExerciseItemsRepository
+    {
+        Task<IEnumerable<WorkoutToExercise>> GetAllWorkoutToExerciseItemsAsync();
+
+        Task WorkoutToExerciseItemsFromWorkout(Workout workout);
+    }
+}
---
+++ src/PersonalRecord.Domain/Repositories/WorkoutToExerciseItemsRepository.cs
@@ -0,0 +1,29 @@
+﻿namespace PersonalRecord.Domain.Repositories
+{
+    using Microsoft.EntityFrameworkCore;
+    using PersonalRecord.Domain.Interfaces;
+    using PersonalRecord.Domain.Models.Entities;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class WorkoutToExerciseItemsRepository : IWorkoutToExerciseItemsRepository
+    {
+        private readonly PersonalRecordContext _context;
+
+        public WorkoutToExerciseItemsRepository(PersonalRecordContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<WorkoutToExercise>> GetAllWorkoutToExerciseItemsAsync()
+        {
+            var workoutToExerciseItems = await _context.WorkoutToExerciseItems.ToListAsync();
+            return workoutToExerciseItems;
+        }
+
+        public async Task WorkoutToExerciseItemsFromWorkout(Workout workout)
+        {
+            await _context.WorkoutToExerciseItems.Where(w => w.WteWorkoutID_FK == workout.WorkoutID).ExecuteDeleteAsync();
+        }
+    }
+}
