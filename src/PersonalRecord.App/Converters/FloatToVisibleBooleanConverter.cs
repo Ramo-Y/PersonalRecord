1dea03f22705fbeeff861f8d4c6e4d0b79f45a57
Ramazan Yilmaz <r.yilmaz@outlook.com>
2024-11-02 16:52:09 +0100

    Add more columns and set visibility dependent on value

---
+++ src/PersonalRecord.App/Converters/FloatToVisibleBooleanConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace PersonalRecord.App.Converters
+{
+    using System.Globalization;
+
+    public class FloatToVisibleBooleanConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is float floatValue && floatValue != 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return Convert(value, targetType, parameter, culture);
+        }
+    }
+}
